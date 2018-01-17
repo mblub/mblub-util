54f93cc3297007c8b1eb992b0f83c224a36dc24f
Mike Blubaugh <mblubaugh@gmail.com>
2018-01-17 08:06:17 -0800

    UncheckedObjectMapper: writeValue to stream, writeValueAsString

--- src/main/java/com/mblub/util/json/unchecked/UncheckedObjectMapper.java
+++ src/main/java/com/mblub/util/json/unchecked/UncheckedObjectMapper.java
@@ -2,10 +2,12 @@ package com.mblub.util.json.unchecked;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.UncheckedIOException;
 import java.io.Writer;
 import java.nio.file.Path;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
@@ -40,10 +42,28 @@ public class UncheckedObjectMapper extends ObjectMapper {
     }
   }
 
+  @Override
+  public void writeValue(OutputStream out, Object value) {
+    try {
+      super.writeValue(out, value);
+    } catch (IOException ioe) {
+      throw new UncheckedIOException(ioe);
+    }
+  }
+
   public void writeValue(Path resultPath, Object value) {
     if (resultPath == null) {
       throw new UncheckedIOException(new IOException("cannot write json value due to null Path"));
     }
     writeValue(resultPath.toFile(), value);
+  }
+
+  @Override
+  public String writeValueAsString(Object value) {
+    try {
+      return super.writeValueAsString(value);
+    } catch (JsonProcessingException jpe) {
+      throw new UncheckedIOException(jpe);
+    }
   }
 }
