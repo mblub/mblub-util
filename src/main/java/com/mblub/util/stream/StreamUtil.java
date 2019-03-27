059a41da265541af6e022156fa2921b6451b252a
Mike Blubaugh <mblubaugh@gmail.com>
2019-03-26 19:04:03 -0700

    streamResourceLines now has signature taking Charset

--- src/main/java/com/mblub/util/stream/StreamUtil.java
+++ src/main/java/com/mblub/util/stream/StreamUtil.java
@@ -1,8 +1,12 @@
 package com.mblub.util.stream;
 
 import java.io.BufferedReader;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.Charset;
 import java.util.Optional;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
@@ -15,8 +19,16 @@ public class StreamUtil {
   }
 
   public static Stream<String> streamResourceLines(String resourcePath) {
+    return streamResourceLines(resourcePath, InputStreamReader::new);
+  }
+
+  public static Stream<String> streamResourceLines(String resourcePath, Charset cs) {
+    return streamResourceLines(resourcePath, in -> new InputStreamReader(in, cs));
+  }
+
+  private static Stream<String> streamResourceLines(String resourcePath, Function<InputStream, Reader> streamToReader) {
     return Optional.ofNullable(resourcePath).filter(StringUtils::isNotEmpty)
-            .map(Thread.currentThread().getContextClassLoader()::getResourceAsStream).map(InputStreamReader::new)
+            .map(Thread.currentThread().getContextClassLoader()::getResourceAsStream).map(streamToReader)
             .map(BufferedReader::new).map(BufferedReader::lines).orElse(Stream.empty());
   }
 }
